bbbe613d56775635f49c96ae726046005a19253f
rahmatginting <32474048+rahmatginting@users.noreply.github.com>
2018-04-21 23:02:20 +0700

    Add Curousel
    Add Curousel

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -242,26 +242,22 @@ private function textMessage($event)
       
     }else if ($questionNum==3) {
       
+      $columns = array();
+      $img_url = "https://res.cloudinary.com/db9zavtws/image/upload/v1486219057/5_ngpkk0.png";
       $menus=$this->tebakkode_m->getMenu($this->resto,$this->categoryID);
       if (is_array($menus) || is_object($menus))
       {
         foreach($menus as $menu) {
-          if(!empty($menu['name']))
-              //$options[] = new MessageTemplateActionBuilder($menu['name'], $menu['name']);
-              //Update progress debug
-			        $test="";            
+          if(!empty($menu['name'])) {
+              //$actions = array(new PostbackTemplateActionBuilder("Add to Cart","action=carousel&button=".$i),
+                //new UriTemplateActionBuilder("View","http://www.google.com"));
+              $actions = array("Pesan","Kembali");
+              $column = new CarouselColumnTemplateBuilder($menu['name'], "description", $img_url , $actions);
+              $columns[] = $column;
+            }
         }
       }
 
-      //pre
-      $columns = array();
-      $img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
-      for($i=0;$i<5;$i++) {
-        $actions = array(new PostbackTemplateActionBuilder("Add to Cart","action=carousel&button=".$i),
-          new UriTemplateActionBuilder("View","http://www.google.com"));
-        $column = new CarouselColumnTemplateBuilder("Title", "description", $img_url , $actions);
-        $columns[] = $column;
-      }
       $carousel = new CarouselTemplateBuilder($columns);
       $messageBuilder = new TemplateMessageBuilder("Carousel Demo", $carousel);
 
@@ -305,18 +301,10 @@ private function textMessage($event)
         
       }else if ($this->user['number']==2) {
 
-        //Update progress debug
-        $this->tebakkode_m->saveProgress('Nomor2');
 
         //get restaurant id
         $this->resto = $this->tebakkode_m->getResto($this->user['user_id']);
-        
-        //Update progress debug
-        $this->tebakkode_m->saveProgress($this->resto);
-
-        //Update progress debug
-        $this->tebakkode_m->saveProgress($message);
-        
+               
         // get Category ID
         $this->categoryID = $this->tebakkode_m->getCategoryID($this->resto, $message);
 
