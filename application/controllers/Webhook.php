eecc59d6b967be74dab7faba95ad54b95ecebe5f
rahmatginting <32474048+rahmatginting@users.noreply.github.com>
2018-04-25 20:14:14 +0700

    update resto desc
    update resto desc

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -290,10 +290,7 @@ private function textMessage($event)
       }
       
       $imageURL="https://myrestobot.herokuapp.com/img/categories.jpg";
-      $alamat = $restoDesc['description'] . ' - ';
-      $alamat .= $restoDesc['address'];
-      $this->tebakkode_m->saveProgress($alamat);
-      $buttonTemplate = new ButtonTemplateBuilder($restoDesc['name'], $alamat, $imageURL, $options);
+      $buttonTemplate = new ButtonTemplateBuilder($restoDesc['name'], $restoDesc['address'], $imageURL, $options);
       //$buttonTemplate = new ButtonTemplateBuilder("Kategori menu", "Pilih kategori menu yang ingin Anda pesan", $imageURL, $options);
                                                   
       // build message
