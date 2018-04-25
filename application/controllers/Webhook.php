5260fbfdc756923d0f54cc6f7fa24d707ade8842
rahmatginting <32474048+rahmatginting@users.noreply.github.com>
2018-04-25 19:44:26 +0700

    Update
    Update

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -267,32 +267,21 @@ private function textMessage($event)
 
       // prepare button template
       //$buttonTemplate = new ButtonTemplateBuilder($question['number']."/10", $question['text'], $question['image'], $options);
-      $this->tebakkode_m->saveProgress('masuk01');
-      $this->tebakkode_m->saveProgress('resto='.$resto);
       $restoDesc=$this->tebakkode_m->getRestoDesc($resto);
-      $this->tebakkode_m->saveProgress('jml='.count($restoDesc));
       
       if (is_array($restoDesc) || is_object($restoDesc)) {
-        $this->tebakkode_m->saveProgress('masukArray');
         if(!empty($restoDesc['name'])) {
-          $this->tebakkode_m->saveProgress('masuk02');
-          $this->tebakkode_m->saveProgress($restoDesc['name']);
-          $this->tebakkode_m->saveProgress('masuk03');
-        }
-        if(!empty($restoDesc['description'])) {
-         $this->tebakkode_m->saveProgress('masuk04');
-         $this->tebakkode_m->saveProgress($restoDesc['description']);
-        }
-        if(!empty($restoDesc['address'])) {
-          $this->tebakkode_m->saveProgress('masuk05');
-          $this->tebakkode_m->saveProgress($restoDesc['address']);
+          if(!empty($restoDesc['description'])) {
+            if(!empty($restoDesc['address'])) {
+              $alamat = $restoDesc['description'] . " " . $restoDesc['address'];
+            }
+          }
         }
       }
       
       $imageURL="https://myrestobot.herokuapp.com/img/categories.jpg";
-      $alamat = $restoDesc['description'] . " " . $restoDesc['address'];
-      //$buttonTemplate = new ButtonTemplateBuilder($restoDesc['name'], $alamat, $imageURL, $options);
-      $buttonTemplate = new ButtonTemplateBuilder("Kategori menu", "Pilih kategori menu yang ingin Anda pesan", $imageURL, $options);
+      $buttonTemplate = new ButtonTemplateBuilder($restoDesc['name'], $alamat, $imageURL, $options);
+      //$buttonTemplate = new ButtonTemplateBuilder("Kategori menu", "Pilih kategori menu yang ingin Anda pesan", $imageURL, $options);
                                                   
       // build message
       $messageBuilder = new TemplateMessageBuilder("Kategori Menu", $buttonTemplate);
