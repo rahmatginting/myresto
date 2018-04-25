07be1f38a29be3951a05fd45158c215159b79cab
rahmatginting <32474048+rahmatginting@users.noreply.github.com>
2018-04-25 19:50:47 +0700

    Update resto desc
    Update resto desc

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -269,7 +269,7 @@ private function textMessage($event)
       //$buttonTemplate = new ButtonTemplateBuilder($question['number']."/10", $question['text'], $question['image'], $options);
       $restoDesc=$this->tebakkode_m->getRestoDesc($resto);
       $this->tebakkode_m->saveProgress('masuk01');
-      $this->tebakkode_m->saveProgress('juml='count($restoDesc));
+      $this->tebakkode_m->saveProgress('juml=' . count($restoDesc));
       
       if (is_array($restoDesc) || is_object($restoDesc)) {
         $this->tebakkode_m->saveProgress('masuk02');
