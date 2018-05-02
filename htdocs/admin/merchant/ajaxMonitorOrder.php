779d0ed8668fbf8e5aa407fee7c0f8690786b5a1
rahmatginting <32474048+rahmatginting@users.noreply.github.com>
2018-05-03 00:14:42 +0700

    update
    update

--- htdocs/admin/merchant/ajaxMonitorOrder.php
+++ htdocs/admin/merchant/ajaxMonitorOrder.php
@@ -1,14 +1,54 @@
 <?php
 include('connect.php');
 
+	$user_sys  = 'user@chatbot.com';
+	$resto_id = '1';
 
-$lastRow = "select id+1 as last_id from restaurant_menu order by id desc limit 1";
-$result = $db->prepare($lastRow);
-$result->execute();
+	//Get notif
+	$notif = 0;
+	$sql = "SELECT count(id) as notif ";
+	$sql .= "FROM menu_order ";
+	$sql .= "WHERE resto_id = '" . $resto_id . "' ";
+	$sql .= "AND DATE(timestamp) = CURRENT_DATE ";
+	$sql .= "AND status = '0' ";
+	$sql .= "GROUP BY timestamp  ";
+	$sql .= "ORDER BY timestamp ASC ";
+	$statement = $db->prepare($sql);
+	$statement->execute();
 
-for($i=0; $row = $result->fetch(); $i++){
- $rowID=$row['last_id'];
-}
+	$statement = $db->prepare($sql);
+	$statement->execute();
 
-echo $rowID;
+	while($row=$statement->fetch(PDO::FETCH_ASSOC)){
+		$notif =  $row['notif'];
+	}
+
+	$sql = "SELECT a.* FROM menu_order a ";
+	$sql .= "WHERE DATE(a.timestamp) = CURRENT_DATE ";
+	$sql .= "AND a.resto_id = '" . $resto_id . "' ";
+	$sql .= "ORDER BY a.timestamp ASC ";
+
+	$statement = $db->prepare($sql);
+	$statement->execute();
+	$someArray = [];
+	while($row=$statement->fetch(PDO::FETCH_ASSOC)){
+		//$details = getDetailOrders($row['id'], $row['resto_id']);
+		$details = 'test';
+	    array_push($someArray, [
+	      'id'   	  => $row['id'],
+	      'table_id'  => $row['table_id'],
+	      'user_name' => $row['user_name'],
+	      'status' 	  => $row['status'],
+	      'details'   => $details,
+	      'notif'     => $notif
+	    ]);
+	}
+
+
+  // Convert the Array to a JSON String and echo it
+  $someJSON = json_encode($someArray);
+  echo $someJSON;
+
+
+
 ?>
