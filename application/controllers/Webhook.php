10d1d22bfebe1f4ed0218ca45a6047581e49fb10
rahmatginting <32474048+rahmatginting@users.noreply.github.com>
2018-05-15 09:04:06 +0700

    perbaikan
    perbaikan

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -223,7 +223,7 @@ private function textMessage($event)
         $msg01 = "Silahkan menunggu petugas kami akan segera melayani Anda";
         $msg02 = "Silahkan klik tombol pilihan dibawah";
         //$buttonTemplate = new ButtonTemplateBuilder($msg01, $msg02, $img_url, $options);
-        $buttonTemplate = new ButtonTemplateBuilder("Terima kasih", "Silahkan klik tombol pilihan dibawah", $img_url, $options);
+        $buttonTemplate = new ButtonTemplateBuilder("Terima kasih", "Petugas kami akan segera melayani Anda", $img_url, $options);
      
         // build message
         $messageBuilder = new TemplateMessageBuilder("Selamat Datang", $buttonTemplate);
@@ -246,7 +246,7 @@ private function textMessage($event)
         $msg01 = "Silahkan menunggu petugas kami akan segera melayani Anda";
         $msg02 = "Silahkan klik tombol pilihan dibawah";
         //$buttonTemplate = new ButtonTemplateBuilder($msg01, $msg02, $img_url, $options);
-        $buttonTemplate = new ButtonTemplateBuilder("Terima kasih", "Silahkan klik tombol pilihan dibawah", $img_url, $options);
+        $buttonTemplate = new ButtonTemplateBuilder("Terima kasih", "Petugas kami akan segera melayani Anda", $img_url, $options);
      
         // build message
         $messageBuilder = new TemplateMessageBuilder("Selamat Datang", $buttonTemplate);
