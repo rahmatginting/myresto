c57faeae676e93931d0b87a0be1d5456233c7456
rahmatginting <32474048+rahmatginting@users.noreply.github.com>
2018-05-16 16:28:13 +0700

    upload file upload
    upload file upload

---
+++ htdocs/admin/merchant/upload.php
@@ -0,0 +1,45 @@
+<?php
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$parent_dir = dirname(dirname($_SERVER['SCRIPT_NAME'])) . '/';
+$imgPath = "image/menu/";
+
+$url = $protocol . $_SERVER['HTTP_HOST'] . $parent_dir . $imgPath;
+
+//$menuID=$_FILES['file']['id'];
+$menuID=$_POST['id'];
+
+// file name
+$filename = $_FILES['file']['name'];
+
+// Location
+$location = '../image/menu/'.$filename;
+
+//Image URL to save into Database
+$imgURL = $url.$filename;
+
+
+// file extension
+$file_extension = pathinfo($location, PATHINFO_EXTENSION);
+$file_extension = strtolower($file_extension);
+
+// Valid image extensions
+$image_ext = array("jpg","png","jpeg","gif");
+
+$response = 0;
+if(in_array($file_extension,$image_ext)){
+	// Upload file
+	if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
+		$response = $imgURL . "|" . $location . "|" . $menuID;
+	}
+}
+
+include('connect.php');
+
+// query
+$sql = "UPDATE restaurant_menu 
+    	SET picture=?
+		WHERE id=?";
+$q = $db->prepare($sql);
+$q->execute(array($imgURL,$menuID));
+
+echo $response;
