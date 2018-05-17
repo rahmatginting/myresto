0ef38a6567564a7ddb7bf012ea6212f03f9928b8
rahmatginting <32474048+rahmatginting@users.noreply.github.com>
2018-05-17 11:54:58 +0700

    protocol
    protocol

--- htdocs/admin/merchant/get_url.php
+++ htdocs/admin/merchant/get_url.php
@@ -1,8 +1,10 @@
 <?php
 $protocol01 = isset($_SERVER["HTTPS"]) ? 'https' : 'http';
 $protocol02 = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$protocol03 = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https'?'https':'http';
 echo "protocol01 = " .$protocol01 ."</br></br>";
 echo "protocol02 = " .$protocol02 ."</br></br>";
+echo "protocol03 = " .$protocol03 ."</br></br>";
 
 $parent_dir = dirname(dirname($_SERVER['SCRIPT_NAME'])) . '/';
 echo "parent_dir = " .$parent_dir ."</br></br>";
