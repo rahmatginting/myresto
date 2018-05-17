c3968e459f97d6d2b675a9cb1dff7c44d0d2273f
rahmatginting <32474048+rahmatginting@users.noreply.github.com>
2018-05-17 11:50:44 +0700

    perbaikan
    perbaikan

--- htdocs/admin/merchant/get_url.php
+++ htdocs/admin/merchant/get_url.php
@@ -1,8 +1,8 @@
 <?php
 $protocol01 = isset($_SERVER["HTTPS"]) ? 'https' : 'http';
 $protocol02 = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-echo "protocol01 = " .$protocol ."</br></br>";
-echo "protocol02 = " .$protocol ."</br></br>";
+echo "protocol01 = " .$protocol01 ."</br></br>";
+echo "protocol02 = " .$protocol02 ."</br></br>";
 
 $parent_dir = dirname(dirname($_SERVER['SCRIPT_NAME'])) . '/';
 echo "parent_dir = " .$parent_dir ."</br></br>";
